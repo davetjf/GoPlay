78aae302b4507d38c83ea3139730d24deb2a0ae8
Dave F <davetjf@gmail.com>
2020-03-22 15:21:18 +0000

    added avatar pic option to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
        protected
 
           def configure_permitted_parameters
-               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password)}
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :avatar_url, :email, :password)}
 
-               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password)}
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :avatar_url, :email, :password, :current_password)}
           end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def avatar_url(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      default_url = "https://sem4project.s3-eu-west-1.amazonaws.com/assets/favicon.PNG"
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{CGI.escape(default_url)}"
+    end
+  end
 end
